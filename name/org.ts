854040a8493379a95af3e00ff1c8f835885b200b
Gnlow <gnlow@protonmail.com>
2022-03-22 00:04:54 +0900

    merge csv

--- name/org.ts
+++ name/org.ts
@@ -9,10 +9,12 @@ const parse = (t: string) => pipe(
     JSON.parse,
     ({data}) => data,
     // as => as.map(({이름, 건수}: any) => [이름, 건수]),
+    /*
     data => stringify(
         data,
         ["이름", "건수"]
     ),
+    */
     p => () => p
 )
 const write =
@@ -24,12 +26,31 @@ const simple =
     pipe(
         () => Deno.readTextFile(`${year}/${name}.json`),
         T.chain(parse),
-        T.chain(write(`${name}/${year}.csv`)),
+        //T.chain(write(`${name}/${year}.csv`)),
     )
 
+const boy: Record<string, any> = {}
+const girl: Record<string, any> = {}
 for await (const dir of Deno.readDir("./")) {
     if (dir.isDirectory && dir.name != "boy" && dir.name != "girl") {
-        await simple("boy", dir.name)()
-        await simple("girl", dir.name)()
+        const boyd = await simple("boy", dir.name)() as any
+        boyd.forEach(({이름, 건수}: any) => {
+            if (! (이름 in boy)) boy[이름] = {}
+            boy[이름][dir.name] = 건수
+        })
+        const girld = await simple("girl", dir.name)() as any
+        girld.forEach(({이름, 건수}: any) => {
+            if (! (이름 in girl)) girl[이름] = {}
+            girl[이름][dir.name] = 건수
+        })
     }
 }
+
+await Deno.writeTextFile("boy.csv",await stringify(
+    Object.entries(boy).map(([name, v]) => ({...v, name})),
+    ["name",...Array(12).fill(void 0).map((x,i)=>String(i+2008))]
+))
+await Deno.writeTextFile("girl.csv",await stringify(
+    Object.entries(girl).map(([name, v]) => ({...v, name})),
+    ["name",...Array(12).fill(void 0).map((x,i)=>String(i+2008))]
+))
